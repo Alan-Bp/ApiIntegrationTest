e5500d9048c0a121dec7e718913e8e85d0b6855b
Alan-Bp <05aabalp@gmail.com>
2025-03-27 07:50:51 -0600

    "ADD - Data Persistence Implementation with Hilt and Room
    - Implemented data persistence functionality using Hilt for dependency injection and Room for local database storage.
    - Modified ArticleRepository to fetch data from the local database or fallback to API.
    - Updated AppModule for proper dependency injection of DBRepository and AppDatabase.
    - Integrated context provision for AppDatabase initialization."

--- app/src/main/java/com/mx/kinedutest/data/di/AppModule.kt
+++ app/src/main/java/com/mx/kinedutest/data/di/AppModule.kt
@@ -1,23 +1,40 @@
 package com.mx.kinedutest.data.di
 
+import android.content.Context
 import com.mx.kinedutest.data.core.Constants
+import com.mx.kinedutest.data.local.dao.ArticleDao
+import com.mx.kinedutest.data.local.database.AppDatabase
 import com.mx.kinedutest.data.remote.ApiService
 import com.mx.kinedutest.data.repository.ArticleRepository
+import com.mx.kinedutest.data.repository.DBRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Provides
-    fun provideArticleRepository(apiService: ApiService): ArticleRepository {
-        return ArticleRepository(apiService)
+    fun provideArticleRepository(
+        apiService: ApiService,
+        dbRepository: DBRepository
+    ): ArticleRepository {
+        return ArticleRepository(apiService, dbRepository)
+    }
+
+    @Provides
+    fun provideDBRepository(articleDao: ArticleDao): DBRepository {
+        return DBRepository(articleDao)
+    }
+
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
     }
 
     @Provides
@@ -29,8 +46,18 @@ object AppModule {
     }
 
     @Provides
-    fun provideApiService(retrofit: Retrofit): ApiService {
-        return retrofit.create(ApiService::class.java)
+    fun provideArticleDao(database: AppDatabase): ArticleDao {
+        return database.articleDao()
     }
 
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return AppDatabase.getInstance(context)
+    }
+
+    @Provides
+    fun provideContext(@ApplicationContext context: Context): Context {
+        return context
+    }
 }
+
